ea6528cafc41a97d4cb14485deaab5a0963ed8fe
yamachi4416 <yamachi4416@gmail.com>
2021-07-23 08:35:33 +0900

    init.rbのdirectoryの設定方法を変更

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 Redmine::Plugin.register :account_lockout do
-  directory File.join(Redmine::Plugin.directory, File.basename(File.dirname(__FILE__)))
+  directory File.join(File.dirname(__FILE__))
   name 'Account Lockout plugin'
   author 'yamachi4416'
   description 'Account Lockout plugin for Redmine'
