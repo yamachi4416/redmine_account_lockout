ae202d86341078a5cb645919e84b1280bcb035ed
yamachi4416 <yamachi4416@gmail.com>
2021-07-23 09:30:46 +0900

    ロックアウト解除のテストを追加

---
+++ test/functional/users_account_lockout_controller_test.rb
@@ -0,0 +1,42 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class UsersAccountLockoutControllerTest < Redmine::ControllerTest
+  fixtures :users, :email_addresses, :roles
+
+  def setup
+    User.current = nil
+    @admin = User.find(1)
+    @jsmith = User.find(2)
+    @dlopper = User.find(3)
+    @request.session[:user_id] = nil
+  end
+
+  def test_without_admin_should_reject
+    @request.session[:user_id] = @dlopper.id
+    post :reset, :params => { :user_id => @jsmith }
+
+    assert_response :forbidden
+  end
+
+  def test_without_admin_should_not_unlockout
+    @jsmith.update_columns(lockout_expired_date: 1.hours.since)
+    assert @jsmith.lockout?
+
+    @request.session[:user_id] = @dlopper.id
+    post :reset, :params => { :user_id => @jsmith }
+
+    @jsmith.reload
+    assert @jsmith.lockout?
+  end
+
+  def test_with_admin_should_unlockout
+    @jsmith.update_columns(lockout_expired_date: 1.hours.since)
+    assert @jsmith.lockout?
+
+    @request.session[:user_id] = @admin.id
+    post :reset, :params => { :user_id => @jsmith }
+
+    @jsmith.reload
+    assert !@jsmith.lockout?
+  end
+end
