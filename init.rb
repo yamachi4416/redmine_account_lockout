96684bc3d529ad57f7915f0247dd07caacf23020
yamachi4416 <yamachi4416@gmail.com>
2021-07-24 02:05:24 +0900

    redmineのバージョン(4.0以上)を指定

--- init.rb
+++ init.rb
@@ -4,6 +4,8 @@ Redmine::Plugin.register :account_lockout do
   author 'yamachi4416'
   description 'Account Lockout plugin for Redmine'
   version '0.0.1'
+  url 'https://github.com/yamachi4416/redmine_account_lockout'
+  requires_redmine version_or_higher: '4.0'
 
   settings partial: 'account_lockout/settings',
     default: {
